6ad6f7661c3fd1a318417e3e2f35a32c6017c9da
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-12 14:36:30 -0300

    feat: added Footer component to Dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { SideBar } from '../SideBar'
 import './styles.scss'
+import { Footer } from '../Footer'
 
 export const Dashboard = () => {
   return (
     <div className="container">
       <SideBar />
+      <main className="main-container">
+        <Footer />
+      </main>
     </div>
   )
 }
