398d04a1480fa1d4feee8e6005d0c4d0e1f44c6a
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-12 14:37:53 -0300

    feat: added function to change tabs on SideBar component & added somes styles

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,32 +1,53 @@
-import React from 'react'
-import tasksIcon from '../../assets/tasks-icon.svg'
+import React, { useState } from 'react'
+import {
+  Task as TaskIcon,
+  Group as GroupIcon,
+  Assignment as AssignmentIcon,
+  Settings as SettingsIcon,
+} from '@mui/icons-material'
+
 import './styles.scss'
 
 export const SideBar = () => {
+  const [currentItemActiveId, setCurrentItemActiveId] = useState<string>('item-1')
+
+  const handleChangeActiveTab = (event: React.MouseEvent<HTMLElement>) => {
+    const currentItemElement = document.getElementById(currentItemActiveId)
+    const itemToActiveElement = document.getElementById(event.currentTarget.id)
+
+    if (currentItemElement?.id === itemToActiveElement?.id) {
+      return
+    }
+
+    if (currentItemElement) {
+      currentItemElement.classList.remove('active')
+    }
+
+    if (itemToActiveElement) {
+      itemToActiveElement.classList.add('active')
+      setCurrentItemActiveId(() => itemToActiveElement.id)
+    }
+  }
+
   return (
     <aside className="side-bar-container">
       <img src="https://www.lognature.com.br/wp-content/uploads/2022/04/logo-log.png" alt="logo of logonature" />
-
       <ul className="side-bar-content">
-        <li className="side-bar-item active">
-          <img src={tasksIcon} alt="icon" />
+        <li id="item-1" className="side-bar-item active" onClick={handleChangeActiveTab}>
+          {<TaskIcon />}
           <span>Tarefas</span>
         </li>
-        <li className="side-bar-item">
-          <img src={tasksIcon} alt="icon" />
-          <span>Tarefas</span>
+        <li id="item-2" className="side-bar-item" onClick={handleChangeActiveTab}>
+          <GroupIcon />
+          <span>Equipes</span>
         </li>
-        <li className="side-bar-item">
-          <img src={tasksIcon} alt="icon" />
-          <span>Tarefas</span>
+        <li id="item-3" className="side-bar-item" onClick={handleChangeActiveTab}>
+          <AssignmentIcon />
+          <span>Relatórios</span>
         </li>
-        <li className="side-bar-item">
-          <img src={tasksIcon} alt="icon" />
-          <span>Tarefas</span>
-        </li>
-        <li className="side-bar-item">
-          <img src={tasksIcon} alt="icon" />
-          <span>Tarefas</span>
+        <li id="item-4" className="side-bar-item" onClick={handleChangeActiveTab}>
+          <SettingsIcon />
+          <span>Ajustes</span>
         </li>
       </ul>
     </aside>
