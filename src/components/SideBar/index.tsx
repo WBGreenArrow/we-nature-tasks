393367ee2754afd0965d430ca5e53befac0f4c16
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-11 23:52:26 -0300

    feat: created SideBar component

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.scss'
+
+export const SideBar = () => {
+  return (
+    <aside className="side-bar-container">
+      <ul>
+        <li>
+          <span>teste</span>
+        </li>
+      </ul>
+    </aside>
+  )
+}
