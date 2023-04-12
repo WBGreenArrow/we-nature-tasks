b52701de97b7728ee29bd54cc54ad72fb86c4453
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-12 00:31:19 -0300

    feat: added item to side bar & styles

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,12 +1,32 @@
 import React from 'react'
+import tasksIcon from '../../assets/tasks-icon.svg'
 import './styles.scss'
 
 export const SideBar = () => {
   return (
     <aside className="side-bar-container">
-      <ul>
-        <li>
-          <span>teste</span>
+      <img src="https://www.lognature.com.br/wp-content/uploads/2022/04/logo-log.png" alt="logo of logonature" />
+
+      <ul className="side-bar-content">
+        <li className="side-bar-item active">
+          <img src={tasksIcon} alt="icon" />
+          <span>Tarefas</span>
+        </li>
+        <li className="side-bar-item">
+          <img src={tasksIcon} alt="icon" />
+          <span>Tarefas</span>
+        </li>
+        <li className="side-bar-item">
+          <img src={tasksIcon} alt="icon" />
+          <span>Tarefas</span>
+        </li>
+        <li className="side-bar-item">
+          <img src={tasksIcon} alt="icon" />
+          <span>Tarefas</span>
+        </li>
+        <li className="side-bar-item">
+          <img src={tasksIcon} alt="icon" />
+          <span>Tarefas</span>
         </li>
       </ul>
     </aside>
