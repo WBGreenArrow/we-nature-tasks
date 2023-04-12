b282e0a15b32d884e21b46637589c3a0e567ad28
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-12 15:21:26 -0300

    feat: change routes from SideBar component

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import {
   Task as TaskIcon,
   Group as GroupIcon,
@@ -10,6 +11,7 @@ import './styles.scss'
 
 export const SideBar = () => {
   const [currentItemActiveId, setCurrentItemActiveId] = useState<string>('item-1')
+  const navigation = useNavigate()
 
   const handleChangeActiveTab = (event: React.MouseEvent<HTMLElement>) => {
     const currentItemElement = document.getElementById(currentItemActiveId)
@@ -24,7 +26,10 @@ export const SideBar = () => {
     }
 
     if (itemToActiveElement) {
+      let router = itemToActiveElement?.getAttribute('router-name')
+
       itemToActiveElement.classList.add('active')
+      navigation(`/${router}`)
       setCurrentItemActiveId(() => itemToActiveElement.id)
     }
   }
@@ -33,19 +38,19 @@ export const SideBar = () => {
     <aside className="side-bar-container">
       <img src="https://www.lognature.com.br/wp-content/uploads/2022/04/logo-log.png" alt="logo of logonature" />
       <ul className="side-bar-content">
-        <li id="item-1" className="side-bar-item active" onClick={handleChangeActiveTab}>
+        <li id="item-1" className="side-bar-item active" router-name="tasks" onClick={handleChangeActiveTab}>
           {<TaskIcon />}
           <span>Tarefas</span>
         </li>
-        <li id="item-2" className="side-bar-item" onClick={handleChangeActiveTab}>
+        <li id="item-2" className="side-bar-item" router-name="teams" onClick={handleChangeActiveTab}>
           <GroupIcon />
           <span>Equipes</span>
         </li>
-        <li id="item-3" className="side-bar-item" onClick={handleChangeActiveTab}>
+        <li id="item-3" className="side-bar-item" router-name="reports" onClick={handleChangeActiveTab}>
           <AssignmentIcon />
           <span>Relatórios</span>
         </li>
-        <li id="item-4" className="side-bar-item" onClick={handleChangeActiveTab}>
+        <li id="item-4" className="side-bar-item" router-name="settings" onClick={handleChangeActiveTab}>
           <SettingsIcon />
           <span>Ajustes</span>
         </li>
