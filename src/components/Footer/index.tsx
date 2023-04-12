3130896199f921537024c9310f628472fa6d9980
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-12 14:35:26 -0300

    feat: created Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.scss'
+
+export const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <h2>Made with ❤️ by</h2>
+      <h3>Wellyson E Brito</h3>
+    </footer>
+  )
+}
