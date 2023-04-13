adf6a4753790c86a2e128580a8e5cd8cbf255676
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-13 01:15:39 -0300

    feat: added change items on FilterSelect component

--- src/components/FilterSelect/index.tsx
+++ src/components/FilterSelect/index.tsx
@@ -1,36 +1,75 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import FilterListIcon from '@mui/icons-material/FilterList'
 import './styles.scss'
 
 export const FilterSelect = () => {
   const [filterValue, setFilterValue] = useState<string>('')
   const [isOpen, setIsOpen] = useState<boolean>(false)
+  const selecteContainerRef = useRef<HTMLUListElement>(null)
+  const prevItemSelectedRef = useRef<HTMLElement | null>(null)
 
-  const handleOpen = () => {
+  useEffect(() => {
+    document.addEventListener('click', closeSelectClickOutside)
+    return () => document.removeEventListener('click', closeSelectClickOutside)
+  }, [])
+
+  useEffect(() => {
+    if (!isOpen) {
+      selecteContainerRef.current?.classList.add('hidden')
+      return
+    }
+    selecteContainerRef.current?.classList.remove('hidden')
+  }, [isOpen])
+
+  const handleOpenSelect = () => {
     setIsOpen((prevState) => !prevState)
   }
 
-  const handleSelectItem = (value: string) => {
-    if (value !== filterValue) {
-      setFilterValue(() => value)
+  const handleSelectItem = (event: React.MouseEvent<HTMLElement>) => {
+    let liElement = event.target as HTMLElement
+    let value = liElement.innerHTML
+
+    handleOpenSelect()
+    if (value === filterValue) {
+      setFilterValue(() => '')
+      liElement.classList.remove('item-active')
+      return
     }
-    handleOpen()
+    setFilterValue(() => value)
+    liElement.classList.add('item-active')
+
+    if (prevItemSelectedRef.current) {
+      prevItemSelectedRef.current.classList.remove('item-active')
+    }
+    prevItemSelectedRef.current = liElement
+  }
+
+  const closeSelectClickOutside = (event: MouseEvent) => {
+    const elementClicked = event.target as HTMLElement
+    if (elementClicked.id === 'filter-button') {
+      return
+    }
+    const isClickInside = selecteContainerRef?.current?.contains(event?.target as Node)
+
+    if (!isClickInside && !selecteContainerRef?.current?.classList.contains('hidden')) {
+      handleOpenSelect()
+    }
   }
 
   return (
     <div className="filter-select-container">
-      <span onClick={handleOpen}>
+      <span id="filter-button" onClick={handleOpenSelect}>
         <FilterListIcon />
-        <label>filter</label>
+        filters
       </span>
-
-      {isOpen && (
-        <ul className="filter-select-items-container">
-          <li onClick={() => handleSelectItem('pending')}>pending</li>
-          <li onClick={() => handleSelectItem('in progress')}>in progress</li>
-          <li onClick={() => handleSelectItem('done')}>done</li>
-        </ul>
-      )}
+      <ul
+        ref={selecteContainerRef as React.LegacyRef<HTMLUListElement>}
+        className="filter-select-items-container hidden"
+      >
+        <li onClick={handleSelectItem}>pending</li>
+        <li onClick={handleSelectItem}>in progress</li>
+        <li onClick={handleSelectItem}>done</li>
+      </ul>
     </div>
   )
 }
