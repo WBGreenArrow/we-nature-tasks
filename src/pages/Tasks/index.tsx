d3579efafda163b7a80d1b152248b2cec53a5df2
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-13 17:33:13 -0300

    feat: added Board component on Tasks page

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { FilterSelect } from '../../components/FilterSelect'
+import { Board } from '../../components/Board'
+import SearchIcon from '@mui/icons-material/Search'
 import './styles.scss'
 
 export const Tasks = () => {
-  return <div>Home</div>
+  return (
+    <div className="tasks-container">
+      <div className="tasks-search-container">
+        <FilterSelect />
+        <span>
+          <SearchIcon />
+          <input type="text" placeholder="Search by cards, subjects or guardians..." />
+        </span>
+      </div>
+      <Board />
+    </div>
+  )
 }
