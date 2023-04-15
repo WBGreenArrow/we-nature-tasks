7d30bb9bf52df0a2c6f04b9e6311fb50f6240089
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-15 12:16:11 -0300

    feat: added PopUpTask componet on Board/component/Card

--- src/components/Board/components/Card/index.tsx
+++ src/components/Board/components/Card/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './styles.scss'
+import { PopUpTask } from '../../../PopUpTask'
 
 export const Card = () => {
+  const [isOpenPopUpTask, setIsOpenPopUpTask] = useState<boolean>(false)
+
+  const handleOpenPopUpTask = (statePopUp: boolean) => {
+    setIsOpenPopUpTask(() => statePopUp)
+  }
+
   return (
-    <div className="card-container">
-      <div className="card-header">
-        <h5>#Bora Codar</h5>
-        <span>13/04/2023</span>
+    <>
+      <div className="card-container" onClick={() => handleOpenPopUpTask(true)}>
+        <div className="card-header">
+          <h5>#Bora Codar</h5>
+          <span>13/04/2023</span>
+        </div>
+        <div className="card-body">
+          <p>Novo desafio do #boraCodar da Rocketseat, onde é proposto construir um quadro de Kanban.</p>
+        </div>
       </div>
-      <div className="card-body">
-        <p>Novo desafio do #boraCodar da Rocketseat, onde é proposto construir um quadro de Kanban.</p>
-      </div>
-    </div>
+      <PopUpTask handleOpen={handleOpenPopUpTask} isOpen={isOpenPopUpTask} />
+    </>
   )
 }
