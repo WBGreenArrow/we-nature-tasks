ce81b77cab1870b07835f5ba6f5f0fb3da4fab61
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-15 18:01:04 -0300

    feat: added mock data to Task page

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -2,19 +2,21 @@ import React from 'react'
 import { Select } from '../../components/Select'
 import { Board } from '../../components/Board'
 import SearchIcon from '@mui/icons-material/Search'
+
+import { data } from './mock'
 import './styles.scss'
 
 export const Tasks = () => {
   return (
     <div className="tasks-container">
       <div className="tasks-search-container">
-        <Select type="filter" />
+        <Select type="filter" onChange={(value) => console.log(value)} />
         <span>
           <SearchIcon />
           <input type="text" placeholder="Search by cards, subjects or guardians..." />
         </span>
       </div>
-      <Board />
+      <Board data={data} />
     </div>
   )
 }
---
+++ src/pages/Tasks/mock.ts
@@ -0,0 +1,77 @@
+export interface ITask {
+  id: number
+  title: string
+  desc: string
+  status: string
+  created_at: string
+  updated_at: string
+}
+
+const data: Array<ITask> = [
+  {
+    id: 1,
+    title: 'Conferir o novo desafio 🚀 ',
+    desc: 'Conferir o novo projeto do #boraCodar para fazê-lo da melhor maneira possível',
+    status: 'pedding',
+    created_at: '2023-15-04',
+    updated_at: '2023-15-04',
+  },
+  {
+    id: 2,
+    title: 'Conferir o novo desafio 🚀 ',
+    desc: 'Conferir o novo projeto do #boraCodar para fazê-lo da melhor maneira possível',
+    status: 'in progress',
+    created_at: '2023-15-04',
+    updated_at: '2023-15-04',
+  },
+  {
+    id: 3,
+    title: 'Conferir o novo desafio 🚀 ',
+    desc: 'Conferir o novo projeto do #boraCodar para fazê-lo da melhor maneira possível',
+    status: 'in progress',
+    created_at: '2023-15-04',
+    updated_at: '2023-15-04',
+  },
+  {
+    id: 4,
+    title: 'Conferir o novo desafio 🚀 ',
+    desc: 'Conferir o novo projeto do #boraCodar para fazê-lo da melhor maneira possível',
+    status: 'in progress',
+    created_at: '2023-15-04',
+    updated_at: '2023-15-04',
+  },
+  {
+    id: 5,
+    title: 'Conferir o novo desafio 🚀 ',
+    desc: 'Conferir o novo projeto do #boraCodar para fazê-lo da melhor maneira possível',
+    status: 'done',
+    created_at: '2023-15-04',
+    updated_at: '2023-15-04',
+  },
+  {
+    id: 6,
+    title: 'Conferir o novo desafio 🚀 ',
+    desc: 'Conferir o novo projeto do #boraCodar para fazê-lo da melhor maneira possível',
+    status: 'done',
+    created_at: '2023-15-04',
+    updated_at: '2023-15-04',
+  },
+  {
+    id: 7,
+    title: 'Conferir o novo desafio 🚀 ',
+    desc: 'Conferir o novo projeto do #boraCodar para fazê-lo da melhor maneira possível',
+    status: 'pedding',
+    created_at: '2023-15-04',
+    updated_at: '2023-15-04',
+  },
+  {
+    id: 8,
+    title: 'Conferir o novo desafio 🚀 ',
+    desc: 'Conferir o novo projeto do #boraCodar para fazê-lo da melhor maneira possível',
+    status: 'pedding',
+    created_at: '2023-15-04',
+    updated_at: '2023-15-04',
+  },
+]
+
+export { data }
