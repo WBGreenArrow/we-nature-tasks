897254c90f7a65c259750802ab06ef03b6a68320
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-17 12:03:52 -0300

    feat: created filterByValue function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,21 @@
+import { ITask } from '../store'
+
 export const orderingId = (array: Array<any>) => {
   const newArrayOrdered = array.map((item, key) => ({ ...item, id: key + 1 }))
   return newArrayOrdered
+}
+
+export const filterByValue = (value: string, taskList: ITask[]) => {
+  const valueFormated = value.toLowerCase()
+  const taskListFiltered = taskList.filter((item) => {
+    const valueToFilter = Object.values(item)
+
+    valueToFilter.shift()
+
+    const itemValue = valueToFilter.join(' ').toLowerCase()
+
+    return itemValue.includes(valueFormated)
+  })
+
+  return taskListFiltered
 }
