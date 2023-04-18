ceb57d660661975a263a9377dec31be63f39764a
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-17 23:14:25 -0300

    fix: fixed filter when input value is empty & added some styles

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,20 +1,23 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { Select } from '../../components/Select'
-import { Board } from '../../components/Board'
-import SearchIcon from '@mui/icons-material/Search'
-
 import { ITask, useStore } from '../../store'
-import './styles.scss'
 import { Status } from '../../constants'
 import { filterByValue } from '../../utils'
+import { Select } from '../../components/Select'
+import { Board } from '../../components/Board'
+import { PopUpTask } from '../../components/PopUpTask'
+
+import { Search as SearchIcon, Add as AddIcon } from '@mui/icons-material'
+
+import './styles.scss'
 
 export const Tasks = () => {
   const [filterValue, setfilterValue] = useState<string>('')
+  const [isOpenPopUpTask, setIsOpenPopUpTask] = useState<boolean>(false)
 
-  const [filterListByStatus, setFilterListByStatus] = useState<ITask[] | []>([])
-  const [pending, setPending] = useState<ITask[] | []>([])
-  const [inProgess, setInProgess] = useState<ITask[] | []>([])
-  const [done, setDone] = useState<ITask[] | []>([])
+  const [filterListByStatus, setFilterListByStatus] = useState<ITask[]>([])
+  const [pending, setPending] = useState<ITask[]>([])
+  const [inProgess, setInProgess] = useState<ITask[]>([])
+  const [done, setDone] = useState<ITask[]>([])
 
   const filterRef = useRef<string>('')
 
@@ -26,6 +29,14 @@ export const Tasks = () => {
 
   const handleOnChangeFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target
+
+    if (value !== '' && !pending.length && !inProgess.length && !done.length) {
+      return
+    }
+
+    if (value !== '' && filterValue !== '' && !filterListByStatus.length) {
+      return
+    }
 
     if (filterValue === '') {
       setPending(() => filterByValue(value, state.pendingTasks))
@@ -62,6 +73,10 @@ export const Tasks = () => {
     }
   }
 
+  const handleOpenPopUpTask = (statePopUp: boolean) => {
+    setIsOpenPopUpTask(() => statePopUp)
+  }
+
   return (
     <div className="tasks-container">
       <div className="tasks-search-container">
@@ -70,6 +85,10 @@ export const Tasks = () => {
           <SearchIcon />
           <input type="text" placeholder="Search by cards, subjects or guardians..." onChange={handleOnChangeFilter} />
         </span>
+        <span onClick={() => handleOpenPopUpTask(true)}>
+          <label> new task</label>
+          <AddIcon />
+        </span>
       </div>
       <Board
         pendingTasks={filterValue !== '' ? filterListByStatus : pending}
@@ -77,6 +96,12 @@ export const Tasks = () => {
         doneTasks={filterValue !== '' ? filterListByStatus : done}
         filterValue={filterValue}
       />
+      {isOpenPopUpTask && (
+        <PopUpTask
+          handleOpen={handleOpenPopUpTask}
+          task={{ title: '', desc: '', status: 'pending', status_list: 'pendingTasks' }}
+        />
+      )}
     </div>
   )
 }
