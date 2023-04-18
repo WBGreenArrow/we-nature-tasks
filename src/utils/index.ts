48b797882964f22d87452a7ff685f36735a406af
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-17 22:25:36 -0300

    feat: created functions to date & encod/ decod base64

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,8 +8,9 @@ export const orderingId = (array: Array<any>) => {
 export const filterByValue = (value: string, taskList: ITask[]) => {
   const valueFormated = value.toLowerCase()
   const taskListFiltered = taskList.filter((item) => {
-    const valueToFilter = Object.values(item)
+    item.updated_at = formatDate(item.updated_at || '')
 
+    const valueToFilter = Object.values(item)
     valueToFilter.shift()
 
     const itemValue = valueToFilter.join(' ').toLowerCase()
@@ -18,4 +19,33 @@ export const filterByValue = (value: string, taskList: ITask[]) => {
   })
 
   return taskListFiltered
+}
+
+export const getCurrentDate = () => {
+  const date = new Date()
+  return date.toISOString()
+}
+
+export const formatDate = (date: string) => {
+  const newDate = new Date(date)
+  const formattedDate = newDate.toLocaleString('en-US', { day: '2-digit', month: '2-digit', year: 'numeric' })
+  return formattedDate
+}
+
+export const utf8ToBase64Encode = (str: string): string => {
+  return window.btoa(
+    encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, (match, p1) => String.fromCharCode(parseInt('0x' + p1, 16))),
+  )
+}
+
+export const base64ToUtf8Decode = (b64: string): string => {
+  return decodeURIComponent(
+    window
+      .atob(b64)
+      .split('')
+      .map((c) => {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+      })
+      .join(''),
+  )
 }
