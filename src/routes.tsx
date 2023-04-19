935e54103f56a7ffff042da12b244362d3a1c142
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-19 16:01:32 -0300

    fix: fixed DashBoard component directory

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import { DashBoard } from './components/DashBboard'
+import { DashBoard } from './components/DashBoard'
 import { Tasks } from './pages/Tasks'
 import { Teams } from './pages/Teams'
 import { Reports } from './pages/Reports'
