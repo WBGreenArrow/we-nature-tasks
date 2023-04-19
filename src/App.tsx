e83af19cee30c8f283356650ae3d32b75af18180
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-19 15:02:03 -0300

    feat: added toast component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import Routes from './routes'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   return (
     <div className="App">
+      <Toaster
+        toastOptions={{
+          style: {
+            fontSize: 16,
+            fontFamily: 'Inter, sans-serif',
+            color: '#1A194D',
+            textTransform: 'capitalize',
+          },
+        }}
+      />
       <Routes />
     </div>
   )
