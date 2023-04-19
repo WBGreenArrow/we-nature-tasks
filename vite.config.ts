b7e3d5e215029033b53b13090a5d87f979e72193
Wellyson Brito <wellysone.brito@gmail.com>
2023-04-19 15:40:58 -0300

    build: config vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr' 
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    svgr()
+    svgr(),
+    reactRefresh()
   ],
 })
